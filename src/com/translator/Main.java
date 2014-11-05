3233a94795bf81a5b7442a3f28fa7bfb9c27c61e
Will_i_am <yadrone@gmail.com>
2014-11-06 01:56:46 +0400

    add read from command lina

--- src/com/translator/Main.java
+++ src/com/translator/Main.java
@@ -5,8 +5,12 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-
-        IOclass iOclass = new IOclass("input.txt", "output.txt");
+        if (args.length < 2) {
+            System.out.println("Write args");
+            System.out.println("usage: <input file> <output file>");
+            System.exit(0);
+        }
+        IOclass iOclass = new IOclass(args[0], args[1]);
         String code = iOclass.read();
         new Lex(code);
         iOclass.close();
