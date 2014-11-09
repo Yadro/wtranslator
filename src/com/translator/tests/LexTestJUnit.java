d6fc87a80bdc283c00695ae8089320fc2a332f70
Will_i_am <yadrone@gmail.com>
2014-11-09 20:11:20 +0300

    Add  tests

--- src/com/translator/tests/LexTestJUnit.java
+++ src/com/translator/tests/LexTestJUnit.java
@@ -1,5 +1,6 @@
 package com.translator.tests;
 
+import com.translator.IOclass;
 import com.translator.Lex;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,18 +11,82 @@ import org.junit.Test;
 public class LexTestJUnit {
 
     private Lex lex;
+    private int state;
 
     @Before
-    public void init() {
-        this.lex = new Lex("if else return lol l12345678 l123456789 ; \n \"");
+    public void start() {
+        new IOclass(null, "output.txt");
     }
 
     @Test
-    public void test() {
+    public void testID() {
+        System.out.println("ID");
+        this.lex = new Lex("ifelse return12", null);
+        if ((state = this.lex.getNext()) != 1) System.out.println("testID faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if ((state = this.lex.getNext()) != 1) System.out.println("testID faild");
+    }
+
+    @Test
+    public void testINT() {
+        this.lex = new Lex("1234567 00000", null);
+        if (this.lex.getNext() != 2) System.out.println("testINT faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 2) System.out.println("testINT faild");
+    }
+
+    @Test
+    public void testIF() {
+        this.lex = new Lex("== != < > <= >=", null);
+        if (this.lex.getNext() != 10) System.out.println("== faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 11) System.out.println("!= faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 14) System.out.println("<  faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 15) System.out.println(">  faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 12) System.out.println("<= faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 13) System.out.println(">= faild");
+    }
+
+    @Test
+    public void testKeywords() {
+        this.lex = new Lex("if int else return", null);
+        if (this.lex.getNext() != 10) System.out.println("if faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 11) System.out.println("int faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 14) System.out.println("else  faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 15) System.out.println("return  faild");
+    }
+
+    @Test
+    public void testOperators() {
+        this.lex = new Lex("+ - = -+ +=", null);
+        if (this.lex.getNext() != 31) System.out.println("+ faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 32) System.out.println("- faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 33) System.out.println("=  faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 32) System.out.println("-  faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 31) System.out.println("+ faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 31) System.out.println("+ faild");
+        if ((state = this.lex.getNext()) != 54) System.out.println("testID faild - split");
+        if (this.lex.getNext() != 33) System.out.println("=  faild");
+    }
+
+    @Test
+    public void testBreckets() {
+        this.lex = new Lex(" \n \t", null);
         int state;
         while ((state = this.lex.getNext()) != -1) {
             System.out.println(state);
         }
     }
-
 }
